2e37d91dfcf9e3d218592a3144451e024b28629b
omcewan <orlandomcewan231@gmail.com>
2022-04-22 06:10:02 -0400

    Header and Navigation component set up

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Nav from '../Navigation'
+
+function Header() {
+  return (
+    <header>
+      <h1>Orlando McEwan</h1>
+      <Nav />
+    </header>
+  );
+}
+
+export default Header;
---
+++ src/components/Navigation/index.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+function Nav() {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <a href="#about">About Me</a>
+        </li>
+        <li>
+          <a href="#portfolio">Portfolio</a>
+        </li>
+        <li>
+          <a href="#contact-me">Contact Me</a>
+        </li>
+        <li>
+          <a href="#resume">Resumé</a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Nav;
