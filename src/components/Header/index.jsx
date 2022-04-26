88584f6fcfc127fa4db9f7d06311b9db9028241d
omcewan <orlandomcewan231@gmail.com>
2022-04-25 21:19:10 -0400

    refactor

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -4,7 +4,7 @@ import Nav from '../Navigation';
 function Header({ navElements, currentNavElement, setCurrentNavElement }) {
   return (
     <header>
-      <a href="/">
+      <a href="#about">
         <h1>Orlando</h1>
       </a>
       {/* <h1>Orlando</h1> */}
