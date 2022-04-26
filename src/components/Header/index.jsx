5ff3520137fbcfae03617a41a6fe9cd217800cdc
omcewan <orlandomcewan231@gmail.com>
2022-04-26 19:48:39 -0400

    added my full name to header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -4,10 +4,7 @@ import Nav from '../Navigation';
 function Header({ navElements, currentNavElement, setCurrentNavElement }) {
   return (
     <header>
-      <a href="#about">
-        <h1>Orlando</h1>
-      </a>
-      {/* <h1>Orlando</h1> */}
+      <h1>Orlando McEwan</h1>
       <Nav
         navElements={navElements}
         currentNavElement={currentNavElement}
